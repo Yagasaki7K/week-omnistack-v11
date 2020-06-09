596a3ce2e6c46de44ee90c4b1d71eda217def062
yagasakii <anderson18_marlon@hotmail.com>
2020-06-09 12:40:05 -0300

    Updates

---
+++ precache-manifest.44b0bda1ab86125e2c7098af6e94c93d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7827ed6c00e827db9970b2326a2933b",
+    "url": "/week-omnistack-v11/index.html"
+  },
+  {
+    "revision": "6cd925a27780895d000a",
+    "url": "/week-omnistack-v11/static/css/main.1a86ad08.chunk.css"
+  },
+  {
+    "revision": "c95c01e7b387124ae61d",
+    "url": "/week-omnistack-v11/static/js/2.f94c7e24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/week-omnistack-v11/static/js/2.f94c7e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd925a27780895d000a",
+    "url": "/week-omnistack-v11/static/js/main.60d079ac.chunk.js"
+  },
+  {
+    "revision": "649ea401c6844ce0288d",
+    "url": "/week-omnistack-v11/static/js/runtime-main.8596d23f.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/week-omnistack-v11/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "62e865e13ce4dd36023d6f8d6c53bb73",
+    "url": "/week-omnistack-v11/static/media/logo.62e865e1.svg"
+  }
+]);
